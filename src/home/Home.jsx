6b479e46d4e09fd1f546aaa39c135137013d1148
adityatadas24 <adityatadas2000@gmail.com>
2024-12-28 16:09:52 +0530

    book-store

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-// import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
 import Freebook from "../components/Freebook";
 import Footer from "../components/Footer";
-import Navbar from "../Components/Navbar";
+
 
 function Home() {
   return (
